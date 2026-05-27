bdfbe6b9a67a6078615e73b0b16539458955df92
K. Daniel Quintero Jimenez <kevindqj10@gmail.com>
2026-05-26 22:47:08 -0500

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -1,13 +1,13 @@
-#Inventario
+#Matriz de inventario
 inventario = [
-    ["A001", "Teclado USB", 8, 15],
-    ["A002", "Mouse Inalámbrico", 20, 10],
-    ["A003", "Monitor 24 pulgadas", 3, 5],
-    ["A004", "Silla Ergonómica", 12, 12],
-    ["A005", "Auriculares Bluetooth",1, 8],
-]
+    ["RF001", "Lapiceros", 10, 30],
+    ["RF002", "Marcadores", 20, 40],
+    ["RF003", "Borradores", 8, 30],
+    ["RF004", "Cuadernos", 40, 70],
+    ["RF005", "Reglas", 15, 50],
+] 
 
-#Módulo: Calcular cantidad a pedir
+#Modulo: Calcular cantidad a pedir
 def calcular_pedido(stock_actual, stock_minimo):
 
     if stock_actual < stock_minimo:
@@ -16,7 +16,7 @@ def calcular_pedido(stock_actual, stock_minimo):
 
 #Auditoría y generación de pedidos
 def auditar_inventario(inventario):
-    print("═══ LISTA DE PEDIDOS ═══")
+    print("Inventario:")
     hay_pedidos = False
 
     for articulo in inventario:
@@ -27,15 +27,10 @@ def auditar_inventario(inventario):
         print(f"Stock minimo: {stock_min}")
 
         if cantidad > 0:
-            print(f"[{codigo}] {nombre}: PEDIR {cantidad} unidades")
+            print(f"[{codigo}] {nombre}: Pedir {cantidad} unidades")
             hay_pedidos = True
         else:
             print(f"[{codigo}] {nombre}: Stock suficiente")
-
-    if not hay_pedidos:
-        print("Todo el inventario está al día.") 
-
-#print(f"{cantidad}") 
 
 auditar_inventario(inventario)
 
